4cb0542f6f36168460a47681c3df07a58a5d5ccc
isailara2 <isailara20002@gmail.com>
2022-05-23 00:46:38 -0500

    Nivel_3_Secuencia_del_juego

--- Level3.java
+++ Level3.java
@@ -2,9 +2,19 @@ import greenfoot.*;
 
 public class Level3 extends World
 {
+    private static int WIDTH_PAPA = 300;
+    private Counter counter = new Counter("Score : ");
+    
+    public void act(){
+        if(counter.getValue()==50){
+            Greenfoot.stop();
+            //Greenfoot.setWorld(new LevelCompletedPage());
+        }
+    }
+    
     public Level3()
     {    
-        super(600, 400, 1); 
+        super(600, 400, 1,false); 
         prepare();
     }
 
@@ -12,5 +22,18 @@ public class Level3 extends World
     {
         RenaRouge renaRouge = new RenaRouge();
         addObject(renaRouge,75,195);
+        
+        addObject(new Tube(), 300, 150);
+        addObject(new Tube(), 600, 150);
+
+        Papa papa = new Papa();
+        
+        for(int x=450;x<10000;x+=WIDTH_PAPA)
+        {
+            papa = new Papa();
+            addObject(papa,x,Greenfoot.getRandomNumber(250)+50);
+        }
+        
+        addObject(counter,536,30);
     }
 }
--- RenaRouge.java
+++ RenaRouge.java
@@ -2,23 +2,60 @@ import greenfoot.*;
 
 public class RenaRouge extends Hero
 {
-    double dy=0;
-    double g=2;
+    private boolean isAlive = true;
+    private double dy=-2;
+    private double gravity=0.5;
+    private static double SPEED=-2;
 
     public RenaRouge(){
         setImage("images/renarouge/rrmov2.png");
     }
-    
+
     public void act()
     {
         moveHeroe();
+        checkCollision();
     }
     
+    public void checkCollision(){
+        if(isTouching(Papa.class)){
+            removeTouching(Papa.class);
+            Counter counter = (Counter) getWorld().getObjects(Counter.class).get(0);
+            counter.add(10);
+        }
+        
+        if(isTouchTube()){
+            isAlive = false;
+        }
+        
+        if(getY()>getWorld().getHeight()){
+            Greenfoot.stop();
+        }
+        
+        if(!isAlive){
+            //Greenfoot.setWorld(new GameOverPage(3));
+            Greenfoot.stop();
+        }
+    }
+
     public void moveHeroe(){
         setLocation(getX(),(int)(getY()+dy));
-        if(Greenfoot.isKeyDown("space")==true){
-            dy=-10;
+        
+        if(Greenfoot.isKeyDown("space")){
+            dy=SPEED;
         }
-        dy=dy+g;
+        
+        dy=dy+gravity;
+    }
+
+    public boolean isTouchTube(){
+        for(Tube tube : getWorld().getObjects(Tube.class)){
+            if(Math.abs(tube.getX() - getX()) < 60 ){
+                if(Math.abs(tube.getY() + 30 - getY()) > 37){
+                    isAlive = false;
+                }
+            }
+        }
+        return !isAlive;
     }
 }
